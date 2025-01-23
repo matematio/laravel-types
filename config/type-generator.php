bd27d4fa5a33d7dca801c443a85df1c3f5cb9636
Robert <robert.papyan@matemat.io>
2025-01-23 18:48:31 +0400

    Readme fix, naming tied to Eloquent models, output file structure change

--- config/type-generator.php
+++ config/type-generator.php
@@ -3,6 +3,7 @@
 return [
     'migrations_path' => 'database/migrations',
     'requests_path' => 'app/Http/Requests',
+    'models_path' => 'app/Models',
     'destination_folder' => 'resources/js/types',
     'blacklist' => [
         '0001_01_01_000000_create_users_table.php', '0001_01_01_000001_create_cache_table.php', '0001_01_01_000002_create_jobs_table.php', '2024_11_14_104342_create_webauthn_credentials.php',
